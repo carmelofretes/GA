506747ab7d670544bc781c9a95a22062e00d76f7
carmelofretes <33060195+carmelofretes@users.noreply.github.com>
2017-12-05 01:21:13 -0300

    05/12/2017 - 01.21

--- ga/src/objects/Solucion.java
+++ ga/src/objects/Solucion.java
@@ -15,8 +15,9 @@ public class Solucion implements Comparable<Solucion>{
     private Double costo;
     private Double saltos;
     private Double espectro;
-    private int pareto;
-    private int indiceRankeo;
+    private int pareto;  //ESTA VARIABLE USO PARA RANKEAR LOS INDIVIDUOS EN CONJUNTOS
+    private int indiceRankeo;  // ESTE INDICE USO PARA PODER BORRAR DESPUES EL INDIVIDUO QUE YA ES NO DOMINADO POR NADIE
+    private double perimetroCuboide; //ESTA VARIABLE ES PARA DETERMINAR EL PERIMETRO DEL CUBOIDE QUE RODEA LA SOLUCION
 
     public Double getSaltos() {
         return saltos;
@@ -34,13 +35,14 @@ public class Solucion implements Comparable<Solucion>{
         this.costo = costo;
     }
 
-    public Solucion(List<Ruteo> ruteos, List<Enlace> enlaces, int cantBloq, Double fitness, int pareto) {
+    public Solucion(List<Ruteo> ruteos, List<Enlace> enlaces, int cantBloq, Double fitness, int pareto, int indiceRankeo, double distanciaCrowding) {
         this.ruteos = ruteos;
         this.enlaces = enlaces;
         this.cantBloq = cantBloq;
         this.fitness = fitness;
         this.pareto = pareto;
         this.indiceRankeo = indiceRankeo ;
+        this.perimetroCuboide = perimetroCuboide ;
     }
 
     public Solucion() {
@@ -50,6 +52,7 @@ public class Solucion implements Comparable<Solucion>{
         fitness = 0.0;
         pareto = -1;
         indiceRankeo = -99;
+        perimetroCuboide = 0.0 ;
     }
 
     public Solucion (List<Enlace> enlaces){
@@ -59,12 +62,27 @@ public class Solucion implements Comparable<Solucion>{
         this.fitness = 0.0;
         this.pareto = -1;
         this.indiceRankeo = -99;
+        this.perimetroCuboide = 0.0 ;
     }
     
+    ////////////////////////////////////////////////////////////////////////////////////
+    //ESTE METODO UTILIZO PARA ORDENAR EL CONJUNTO DE PARETO, POR LA VARIABLE "PARETO"
+    ////////////////////////////////////////////////////////////////////////////////////
     @Override
     public int compareTo(Solucion s) {
+        //PARA ORDENAR EN RANKEO DE PARETO
         if (pareto < s.pareto) { return -1; }
         if (pareto > s.pareto) { return 1 ; }
+/*        //PARA ORDENAR POR COSTO, CUANDO NECESITE CALCULAR LA DISTANCIA DE CROWDING
+        if (costo < s.costo) { return -1; }
+        if (costo > s.costo) { return 1 ; } 
+        //PARA ORDENAR POR SALTO, CUANDO SE NECESITE CALCULAR LA DISTANCIA DE CROWDING
+        if (saltos < s.saltos) { return -1; }
+        if (saltos > s.saltos) { return 1 ; } 
+        //PARA ORDENAR POR ESPECTRO, CUANDO SE NECESITE CALCULAR LA DISTANCIA DE CROWDING
+        if (espectro < s.espectro) { return -1; }
+        if (espectro > s.espectro) { return 1 ; } */
+        
         return 0;
     }
 
@@ -90,6 +108,14 @@ public class Solucion implements Comparable<Solucion>{
 
     public void setPareto(int pareto) {
         this.pareto = pareto;
+    }
+    
+    public double getPerimetroCuboide() {
+        return perimetroCuboide ;
+    }
+    
+    public void setPerimetroCuboide(double perimetroCuboide) {
+        this.perimetroCuboide = perimetroCuboide ;
     }
 
     public List<Ruteo> getRuteos() {
@@ -122,6 +148,5 @@ public class Solucion implements Comparable<Solucion>{
 
     public void setIndiceRankeo(int indiceRankeo) {
         this.indiceRankeo = indiceRankeo;
-    }    
-
+    }  
 }
